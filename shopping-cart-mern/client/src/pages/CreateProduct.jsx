19f28964a92568893ada7a8e5bebe0b6d272c45a
dbhartra <devbhartra183@gmail.com>
2023-04-29 08:02:44 +0530

    Updated create product

--- shopping-cart-mern/client/src/pages/CreateProduct.jsx
+++ shopping-cart-mern/client/src/pages/CreateProduct.jsx
@@ -1,31 +1,34 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Col, Row } from "react-bootstrap";
 
 function CreateProduct() {
     return (
         <Container>
-            <h1>Create Product</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="formName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="name" placeholder="Enter product name" />
-                    <Form.Text className="text-muted">
-                        Enter the name of the product.
-                    </Form.Text>
-                </Form.Group>
+            <Row>
+                <Col></Col>
+                <Col xs={6}>
+                    <h1>Create Product</h1>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Enter product name" />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formCost">
-                    <Form.Label>Cost</Form.Label>
-                    <Form.Control type="number" placeholder="Enter the cost" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formDescription">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control type="text" placeholder="Enter the description" rows = {3}/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                        <Form.Group className="mb-3" controlId="formCost">
+                            <Form.Label>Cost</Form.Label>
+                            <Form.Control type="number" placeholder="Enter the cost" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formDescription">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control type="text" placeholder="Enter the description" rows={3} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+                <Col></Col>
+            </Row>
         </Container>
     );
 }
